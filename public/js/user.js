b0e2b81c465a033f117cf254d6fa19d3177b5da3
Cesar Augusto <cesar.guga2013@gmail.com>
2020-06-24 18:14:56 -0300

    Alterando animações na pagina de usuário e login

--- public/js/user.js
+++ public/js/user.js
@@ -10,8 +10,8 @@ function scrollDicas() {
     let body = document.body;
     let html = document.documentElement;
 
-    body.scrollTop = 1050;
-    html.scrollTop = 1050;
+    body.scrollTop = 2350;
+    html.scrollTop = 2350;
 }
 
 function scrollContato() {
@@ -20,4 +20,18 @@ function scrollContato() {
 
     body.scrollTop = 10000;
     html.scrollTop = 10000;
+}
+
+window.onscroll = function () { verificaScroll() }
+
+function verificaScroll() {
+    let body = document.body;
+    let html = document.documentElement;
+
+    if (body.scrollTop > 2150 || html.scrollTop > 2150) {
+        tips_devs.style.display = 'flex'
+    }
+    if (body.scrollTop > 2750 || html.scrollTop > 2750) {
+        div_sugestions.style.display = 'inline-block'
+    }
 }
